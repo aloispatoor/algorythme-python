4f8b32eb82fda410a1a674a3250277844d35c726
Alois Patoor <palois89@posteo.net>
2022-10-18 14:50:31 +0200

    Exercice 7 edited

--- ex7.py
+++ ex7.py
@@ -3,9 +3,11 @@ inpoute = int(input("Indiquez votre note: "))
 num = []
 
 # FINIT LA BOUCLE EN INPUT -1
-while inpoute >= 0:
+while inpoute != -1:
     num.append(inpoute)
     inpoute = int(input("Indiquez votre note: "))
+
+print(num)
 
 # FONCTION
 def moyenne(table :list[int]):
@@ -13,7 +15,8 @@ def moyenne(table :list[int]):
     somme = 0
     for i in table:
         somme = somme + i
-    return somme / i
+    average = somme / len(table)
+    return int(average)
 
 result = moyenne(num)
 print("la moyenne est de " + str(result))
