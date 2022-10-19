ecdd105f7476557414474410855758a800fc660c
Alois Patoor <palois89@posteo.net>
2022-10-19 10:29:47 +0200

    finishing exercices

--- exo8.py
+++ exo8.py
@@ -1,45 +1,40 @@
 class Student:
-    name = ""
-    surname = ""
-    note = []
 
-    def __init__(self, name : str, surname : str, note : int):
+    def __init__(self, name : str, surname : str, note : list[int]):
         self.name = name
         self.surname = surname
         self.note = note
 
-    def studentAverage(self, note :list[int]) -> int:
+    def studentAverage(self) -> int:
         i = len(self.note)
-        sum = 0
-        for i in note:
-            sum = sum + i
-        average = sum / i
-        return average
+        return sum(self.note)/i
 
 class Classe:
-    listStudent = []
 
-    def __init__(self) -> None:
-        pass
-    
-    def addStudent(self, student : Student):
-        self.listStudent.append(student)
+    def __init__(self, name : str, listStudent : list[Student]):
+        self.name = name
+        self.listStudent = listStudent
 
-    def classeAverage(self, studentsAverage : list[int]) -> int:
-        i = len(self.listStudent)
+    def classeAverage(self) -> int:
         sum = 0
-        for i in studentsAverage:
-            sum = sum + i
-        average = sum / i
-        return average
+        for i in self.listStudent:
+            sum = sum + i.studentAverage()
+        return sum / len(self.listStudent)
+
 
 Robert = Student("Gobaert", "Robert", [9, 15, 17, 13, 12, 8])
 Theo = Student("Ravi", "Théo", [7, 8, 17, 16, 4, 11])
 
-averageRobert = Student.studentAverage(Robert)
-averageTheo = Student.studentAverage(Theo)
+cm1 = Classe("CM1", [Robert, Theo])
+
+averageRobert = Robert.studentAverage()
 print(f"La moyenne de {Robert.surname} est de {averageRobert}")
+
+averageTheo = Theo.studentAverage()
 print(f"La moyenne de {Theo.surname} est de {averageTheo}")
+
+averageCm1 = cm1.classeAverage()
+print(f"La moyenne de la classe {cm1.name} est de {averageCm1}")
 
 
     
